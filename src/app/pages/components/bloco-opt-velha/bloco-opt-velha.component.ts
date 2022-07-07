2315ba0194ebc8c869b7e80936ad798507b07a83
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2022-07-07 15:00:29 -0300

    pagina compilada

--- src/app/pages/components/bloco-opt-velha/bloco-opt-velha.component.ts
+++ src/app/pages/components/bloco-opt-velha/bloco-opt-velha.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BlocoOptVelhaComponent implements OnInit {
 
-  @Input() value: 'X' | 'O';
+  @Input() value: string;
   constructor() { }
 
 
